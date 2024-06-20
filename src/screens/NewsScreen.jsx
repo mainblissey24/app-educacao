667cbeec1c99613de2b65222f8648ef45e434416
Patrick <115029356+mainblissey24@users.noreply.github.com>
2024-06-20 13:44:28 -0300

    news_quase_funcionando

--- src/screens/NewsScreen.jsx
+++ src/screens/NewsScreen.jsx
@@ -1,10 +1,41 @@
+import { FlatList, Text, View } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { styles } from "../config/styles";
 
 
+export default function NewsScreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Lista de Produtos</Text>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.productItem}>
+            <Text style={styles.productName}>{item.name}</Text>
+            <Text style={styles.productPrice}>Preço: ${item.price}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+
+/*** 
 import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
 
-export default function NewsScreen () {
+export default function NewsScreen () => {
   const [dados, setDados] = useState([]);
 
   useEffect(() => {
@@ -24,4 +55,5 @@ export default function NewsScreen () {
       ))}
     </View>
   );
-};
+}; 
+**/
